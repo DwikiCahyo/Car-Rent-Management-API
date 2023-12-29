e00326691f088c9fc693de8fa203041043aadf50
DwikiCahyo <cbudi35@gmail.com>
2023-12-29 10:30:23 +0700

    fix : deployment

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import App from "./server";
 const port = process.env.PORT;
 
+console.log(process.env.DATABASE_URL);
+
 const server = new App().app;
 server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
