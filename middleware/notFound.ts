983d580c1cd8fb5396bf2a19393b478444658db4
DwikiCahyo <cbudi35@gmail.com>
2023-12-29 10:02:58 +0700

    add : deployment

--- middleware/notFound.ts
+++ middleware/notFound.ts
@@ -1,8 +1,12 @@
-import { NextFunction, Request, Response } from "express";
+import { type NextFunction, type Request, type Response } from "express";
 
-export function NotFoundError(req: Request, res: Response, next: NextFunction) {
+export function NotFoundError(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
   res.locals.errorMessage = "Not Found";
   res.status(404);
-  res.json({ error: 404 });
+  res.json({ error: 404, message: "Not Found " });
   next();
 }
